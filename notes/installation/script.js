dbb74f3ef52667184e95d685dc247112ae6115d8
Enki Layman <layman212@gmail.com>
2023-03-26 01:31:42 -0400

    finished installation section

---
+++ notes/installation/script.js
@@ -0,0 +1,2 @@
+//React was designed for gradual adaptation. You can use as little or as much React as youd like in your app.
+
---
+++ notes/start a new react project/script.js
@@ -0,0 +1,4 @@
+//If you want build an app fully with React it is recommended now to use a React framework, such as Next.js
+//Frameworks provide features which most apps will eventually need, such as routing, data fetching, and generating HTML.
+
+//You should look at editor setup when starting new projects.
